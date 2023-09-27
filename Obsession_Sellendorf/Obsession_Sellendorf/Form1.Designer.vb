fc35a992f41f1b6debdeb7f3984bdf689bf94e37
Herbert Sauber <hs@pisam.de>
2023-09-27 18:56:28 +0200

    erste Version

---
+++ Obsession_Sellendorf/Obsession_Sellendorf/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.tbR1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(29, 42)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(244, 60)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Choose Switch"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(551, 48)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(208, 31)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(29, 122)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(244, 58)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Connect"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(29, 224)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(114, 42)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Relaiy 1"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'tbR1
+        '
+        Me.tbR1.BackColor = System.Drawing.SystemColors.InactiveCaption
+        Me.tbR1.Location = New System.Drawing.Point(191, 230)
+        Me.tbR1.Name = "tbR1"
+        Me.tbR1.Size = New System.Drawing.Size(116, 31)
+        Me.tbR1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.tbR1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents tbR1 As TextBox
+End Class
---
+++ Obsession_Sellendorf/Obsession_Sellendorf/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private objDome As ASCOM.DriverAccess.Dome
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim obj As New ASCOM.Utilities.Chooser
+        obj.DeviceType = "Dome"
+        TextBox1.Text = obj.Choose(TextBox1.Text)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        objDome = New ASCOM.DriverAccess.Dome(TextBox1.Text)
+        objDome.Connected = True
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'objSwitch.SetSwitch(1, True)
+        tbR1.BackColor = Color.Green
+    End Sub
+End Class
